e0c25427bae47078f99d1287e0962d09586cde77
opqrstuvcut <develop@Macintosh.local>
2021-07-21 15:47:16 +0900

    fix extention type

--- ios/Classes/SwiftAmazonS3CognitoPlugin.swift
+++ ios/Classes/SwiftAmazonS3CognitoPlugin.swift
@@ -79,194 +79,190 @@ public class SwiftAmazonS3CognitoPlugin: NSObject, FlutterPlugin {
                 contentType = contentTypeParam!
             }
 
-        if(contentTypeParam == nil || contentTypeParam!.count == 0 &&  fileName!.contains(".")){
-            var index = fileName!.lastIndex(of: ".")
-                index = fileName!.index(index!, offsetBy: 1)
-                if(index != nil){
-                    let extention = String(fileName![index!...])
-                        print("extension"+extention);
-                    if(extention.lowercased().contains("png") ||
-                            extention.lowercased().contains("jpg") ||
-                            extention.lowercased().contains("jpeg") ){
-                        contentType = "image/"+extention
-                    }else{
 
-                        if(extention.lowercased().contains("pdf")){
-                            contentType = "application/pdf"
-                        }else{
+              if(contentTypeParam == nil || contentTypeParam!.count == 0 &&  fileName!.contains(".")){
+                             var index = fileName!.lastIndex(of: ".")
+                             index = fileName!.index(index!, offsetBy: 1)
+                             if(index != nil){
+                                 let extention = String(fileName![index!...])
+                                 print("extension"+extention);
+                                 if(extention.lowercased().contains("png") ||
+                                 extention.lowercased().contains("jpg") ||
+                                     extention.lowercased().contains("jpeg") ){
+                                     contentType = "image/"+extention
+                                 }else{
 
-                            if(extention.lowercased().contains("mp4")){
-                                contentType = "video/mp4"
-                            }else{
-                                contentType = "application/*"
-                            }
+                                  if(extention.lowercased().contains("mp4")){
+                                      contentType = "video/mp4"
+                                      }else{
+                                      contentType = "application/*"
+                                      }
 
-                        }
+                                 }
 
+                             }
+                         }
+
+            uploadRequest?.contentType = contentType
+
+                uploadRequest?.body = fileUrl as URL
+
+                uploadRequest?.acl = .private
+
+
+
+
+                AWSS3TransferManager.default().upload(uploadRequest!).continueWith { (task) -> AnyObject? in
+                    if let error = task.error {
+                        print("❌ Upload failed (\(error))")
                     }
+
+
+                    if task.result != nil {
+
+                        //                        imageAmazonUrl = "https://s3-" + self.subRegion1.stringValue +  ".amazonaws.com/\(bucket!)/\(uploadRequest!.key!)"
+                        //
+                        imageAmazonUrl = AWSS3.default().configuration.endpoint.url.description + "/\(bucket!)/\(uploadRequest!.key!)"
+
+                            print("✅ Upload successed (\(imageAmazonUrl))")
+                    } else {
+                        print("❌ Unexpected empty result.")
+                    }
+                    result(imageAmazonUrl)
+                        return nil
                 }
         }
 
-        uploadRequest?.contentType = contentType
+        func deleteImage(_ call: FlutterMethodCall, result: @escaping FlutterResult){
+            let arguments = call.arguments as? NSDictionary
+                let bucket = arguments!["bucket"] as? String
+                let identity = arguments!["identity"] as? String
+                let fileName = arguments!["imageName"] as? String
+                let region = arguments!["region"] as? String
+                let subRegion = arguments!["subRegion"] as? String
 
-            uploadRequest?.body = fileUrl as URL
 
-            uploadRequest?.acl = .private
-
-
-
-
-            AWSS3TransferManager.default().upload(uploadRequest!).continueWith { (task) -> AnyObject? in
-                if let error = task.error {
-                    print("❌ Upload failed (\(error))")
+                if(region != nil && subRegion != nil){
+                    initRegions(region: region!, subRegion: subRegion!)
                 }
 
+            let credentialsProvider = AWSCognitoCredentialsProvider(
+                    regionType: region1,
+                    identityPoolId: identity!)
+                let configuration = AWSServiceConfiguration(
+                        region: subRegion1,
+                        credentialsProvider: credentialsProvider)
+                AWSServiceManager.default().defaultServiceConfiguration = configuration
 
-                if task.result != nil {
-
-                    //                        imageAmazonUrl = "https://s3-" + self.subRegion1.stringValue +  ".amazonaws.com/\(bucket!)/\(uploadRequest!.key!)"
-                    //
-                    imageAmazonUrl = AWSS3.default().configuration.endpoint.url.description + "/\(bucket!)/\(uploadRequest!.key!)"
-
-                        print("✅ Upload successed (\(imageAmazonUrl))")
-                } else {
-                    print("❌ Unexpected empty result.")
-                }
-                result(imageAmazonUrl)
-                    return nil
-            }
-    }
-
-    func deleteImage(_ call: FlutterMethodCall, result: @escaping FlutterResult){
-        let arguments = call.arguments as? NSDictionary
-            let bucket = arguments!["bucket"] as? String
-            let identity = arguments!["identity"] as? String
-            let fileName = arguments!["imageName"] as? String
-            let region = arguments!["region"] as? String
-            let subRegion = arguments!["subRegion"] as? String
-
-
-            if(region != nil && subRegion != nil){
-                initRegions(region: region!, subRegion: subRegion!)
-            }
-
-        let credentialsProvider = AWSCognitoCredentialsProvider(
-                regionType: region1,
-                identityPoolId: identity!)
-            let configuration = AWSServiceConfiguration(
-                    region: subRegion1,
-                    credentialsProvider: credentialsProvider)
-            AWSServiceManager.default().defaultServiceConfiguration = configuration
-
-            AWSS3.register(with: configuration!, forKey: "defaultKey")
-            let s3 = AWSS3.s3(forKey: "defaultKey")
-            let deleteObjectRequest = AWSS3DeleteObjectRequest()
-            deleteObjectRequest?.bucket = bucket // bucket name
-            deleteObjectRequest?.key = fileName // File name
-            s3.deleteObject(deleteObjectRequest!).continueWith { (task:AWSTask) -> AnyObject? in
-                if let error = task.error {
-                    print("Error occurred: \(error)")
-                        result("Error occurred: \(error)")
-                        return nil
-                }
-                print("image deleted successfully.")
-                    result("image deleted successfully.")
-                    return nil
-            }
-
-
-    }
-
-    func listFiles(_ call: FlutterMethodCall, result: @escaping FlutterResult){
-        let arguments = call.arguments as? NSDictionary
-            let bucket = arguments!["bucket"] as? String
-            let identity = arguments!["identity"] as? String
-            let filePrefix = arguments!["prefix"] as? String
-            let region = arguments!["region"] as? String
-            let subRegion = arguments!["subRegion"] as? String
-
-
-            if(region != nil && subRegion != nil){
-                initRegions(region: region!, subRegion: subRegion!)
-            }
-
-        let credentialsProvider = AWSCognitoCredentialsProvider(
-                regionType: AWSRegionType.regionTypeForString(regionString: region!),
-                identityPoolId: identity!)
-            let configuration = AWSServiceConfiguration(
-                    region: AWSRegionType.regionTypeForString(regionString: subRegion!),
-                    credentialsProvider: credentialsProvider)
-            AWSServiceManager.default().defaultServiceConfiguration = configuration
-
-            AWSS3.register(with: configuration!, forKey: "defaultKey")
-
-            let s3 = AWSS3.s3(forKey: "defaultKey")
-            let listRequest = AWSS3ListObjectsRequest()
-            listRequest?.bucket = bucket // bucket name
-            listRequest?.prefix = filePrefix // File prefix
-
-            s3.listObjects(listRequest!).continueWith { (task:AWSTask) -> AnyObject? in
-                if let error = task.error {
-                    print("Error occurred: \(error)")
-                        result("Error occurred: \(error)")
+                AWSS3.register(with: configuration!, forKey: "defaultKey")
+                let s3 = AWSS3.s3(forKey: "defaultKey")
+                let deleteObjectRequest = AWSS3DeleteObjectRequest()
+                deleteObjectRequest?.bucket = bucket // bucket name
+                deleteObjectRequest?.key = fileName // File name
+                s3.deleteObject(deleteObjectRequest!).continueWith { (task:AWSTask) -> AnyObject? in
+                    if let error = task.error {
+                        print("Error occurred: \(error)")
+                            result("Error occurred: \(error)")
+                            return nil
+                    }
+                    print("image deleted successfully.")
+                        result("image deleted successfully.")
                         return nil
                 }
 
-                let keys = task.result?.contents!.map({ $0.key })
-                    result(keys)
-                    return nil
-            }
 
-    }
-
-    public func initRegions(region:String,subRegion:String){
-        region1 = getRegion(name: region)
-            subRegion1 = getRegion(name: subRegion)
-    }
-
-    public func getRegion( name:String ) -> AWSRegionType{
-
-        if(name == "US_EAST_1"){
-            return AWSRegionType.USEast1
-        }else if(name == "AP_SOUTHEAST_1"){
-            return AWSRegionType.APSoutheast1
-        }else if(name == "US_EAST_2"){
-            return AWSRegionType.USEast2
-        }else if(name == "EU_WEST_1"){
-            return AWSRegionType.EUWest1
-        }else if(name == "CA_CENTRAL_1"){
-            return AWSRegionType.CACentral1
-        }else if(name == "CN_NORTH_1"){
-            return AWSRegionType.CNNorth1
-        } else if(name == "CN_NORTHWEST_1"){
-            return AWSRegionType.CNNorthWest1
-        }else if(name == "EU_CENTRAL_1"){
-            return AWSRegionType.EUCentral1
-        } else if(name == "EU_WEST_2"){
-            return AWSRegionType.EUWest2
-        }else if(name == "EU_WEST_3"){
-            return AWSRegionType.EUWest3
-        } else if(name == "SA_EAST_1"){
-            return AWSRegionType.SAEast1
-        } else if(name == "US_WEST_1"){
-            return AWSRegionType.USWest1
-        }else if(name == "US_WEST_2"){
-            return AWSRegionType.USWest2
-        } else if(name == "AP_NORTHEAST_1"){
-            return AWSRegionType.APNortheast1
-        } else if(name == "AP_NORTHEAST_2"){
-            return AWSRegionType.APNortheast2
-        } else if(name == "AP_SOUTHEAST_1"){
-            return AWSRegionType.APSoutheast1
-        }else if(name == "AP_SOUTHEAST_2"){
-            return AWSRegionType.APSoutheast2
-        } else if(name == "AP_SOUTH_1"){
-            return AWSRegionType.APSouth1
-        }else if(name == "ME_SOUTH_1"){
-            return AWSRegionType.MESouth1
         }
 
-        return AWSRegionType.Unknown
+        func listFiles(_ call: FlutterMethodCall, result: @escaping FlutterResult){
+            let arguments = call.arguments as? NSDictionary
+                let bucket = arguments!["bucket"] as? String
+                let identity = arguments!["identity"] as? String
+                let filePrefix = arguments!["prefix"] as? String
+                let region = arguments!["region"] as? String
+                let subRegion = arguments!["subRegion"] as? String
 
+
+                if(region != nil && subRegion != nil){
+                    initRegions(region: region!, subRegion: subRegion!)
+                }
+
+            let credentialsProvider = AWSCognitoCredentialsProvider(
+                    regionType: AWSRegionType.regionTypeForString(regionString: region!),
+                    identityPoolId: identity!)
+                let configuration = AWSServiceConfiguration(
+                        region: AWSRegionType.regionTypeForString(regionString: subRegion!),
+                        credentialsProvider: credentialsProvider)
+                AWSServiceManager.default().defaultServiceConfiguration = configuration
+
+                AWSS3.register(with: configuration!, forKey: "defaultKey")
+
+                let s3 = AWSS3.s3(forKey: "defaultKey")
+                let listRequest = AWSS3ListObjectsRequest()
+                listRequest?.bucket = bucket // bucket name
+                listRequest?.prefix = filePrefix // File prefix
+
+                s3.listObjects(listRequest!).continueWith { (task:AWSTask) -> AnyObject? in
+                    if let error = task.error {
+                        print("Error occurred: \(error)")
+                            result("Error occurred: \(error)")
+                            return nil
+                    }
+
+                    let keys = task.result?.contents!.map({ $0.key })
+                        result(keys)
+                        return nil
+                }
+
+        }
+
+        public func initRegions(region:String,subRegion:String){
+            region1 = getRegion(name: region)
+                subRegion1 = getRegion(name: subRegion)
+        }
+
+        public func getRegion( name:String ) -> AWSRegionType{
+
+            if(name == "US_EAST_1"){
+                return AWSRegionType.USEast1
+            }else if(name == "AP_SOUTHEAST_1"){
+                return AWSRegionType.APSoutheast1
+            }else if(name == "US_EAST_2"){
+                return AWSRegionType.USEast2
+            }else if(name == "EU_WEST_1"){
+                return AWSRegionType.EUWest1
+            }else if(name == "CA_CENTRAL_1"){
+                return AWSRegionType.CACentral1
+            }else if(name == "CN_NORTH_1"){
+                return AWSRegionType.CNNorth1
+            } else if(name == "CN_NORTHWEST_1"){
+                return AWSRegionType.CNNorthWest1
+            }else if(name == "EU_CENTRAL_1"){
+                return AWSRegionType.EUCentral1
+            } else if(name == "EU_WEST_2"){
+                return AWSRegionType.EUWest2
+            }else if(name == "EU_WEST_3"){
+                return AWSRegionType.EUWest3
+            } else if(name == "SA_EAST_1"){
+                return AWSRegionType.SAEast1
+            } else if(name == "US_WEST_1"){
+                return AWSRegionType.USWest1
+            }else if(name == "US_WEST_2"){
+                return AWSRegionType.USWest2
+            } else if(name == "AP_NORTHEAST_1"){
+                return AWSRegionType.APNortheast1
+            } else if(name == "AP_NORTHEAST_2"){
+                return AWSRegionType.APNortheast2
+            } else if(name == "AP_SOUTHEAST_1"){
+                return AWSRegionType.APSoutheast1
+            }else if(name == "AP_SOUTHEAST_2"){
+                return AWSRegionType.APSoutheast2
+            } else if(name == "AP_SOUTH_1"){
+                return AWSRegionType.APSouth1
+            }else if(name == "ME_SOUTH_1"){
+                return AWSRegionType.MESouth1
+            }
+
+            return AWSRegionType.Unknown
+
+        }
     }
-}
